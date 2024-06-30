0d8f843c010e5869c5dfbf8981baddde42023a37
toribird-2048 <fly-toribird@outlook.jp>
2024-06-30 14:30:39 +0900

    2024/6/30 14:30

--- main.py
+++ main.py
@@ -1,16 +1,79 @@
-class Formula :
-    def __init__(self,*coef) :
+import random
+
+levelup_cost = 1
+mutation_rate = 0.5
+mutation_dim_range = 2
+mutaiton_coef_range = 2
+
+class Formula() :
+    def __init__(self,formula) :
         """
-        coefficient:係数
-        coef:[xの係数,xの次数]
+        coefficient,coef:係数
+        dimension,dim:次数
+        formula:{dim1:coef1,dim2:coef2,......}
         """
-        self.coef = list(coef)
-    
+        self.__formula:dict = formula
+
     def calc(self,x) :
         ans = 0
-        for k in range(len(self.coef)) :
-            ans += self.coef[k][0] * (x ** self.coef[k][1])
+        for dim,coef in self.__formula.items() :
+            ans += coef * (x ** dim)
         return ans
 
-F = Formula([2,0],[4,1])
-print(F.calc(0))
+    def get_formula(self) :
+        return self.__formula
+    
+    def get_dim(self) :
+        return list(self.__formula.keys())
+
+    def get_coef(self,dim=all) :
+        if dim == all :
+            return list(self.__formula.values())
+        elif dim in self.__formula.keys() :
+            return self.__formula[dim]
+        else :
+            return
+        
+    def add_term(self,dim,coef) :
+        if dim in self.__formula.keys() :
+            self.__formula[dim] += coef
+        else :
+            self.__formula[dim] = coef
+
+    def del_term(self,dim) :
+        if dim in self.__formula.keys() :
+            del self.__formula[dim]
+
+    def zero_check(self):
+        for coef in self.__formula.values() :
+            if coef == 0 :
+                self.del_term(self.__formula.keys()[self.__formula.values().index(coef)])
+    
+class Earth() :
+    def __init__(self,limit,sample) :
+        self.__limit:int = limit
+        self.__list = []
+        self.__sample:Formula = sample
+
+    def randomGen(self,dim_min,dim_max,coef_min,coef_max) :
+        terms = {}
+        for k in range(dim_min,dim_max + 1) :
+            terms[k] = random.uniform(coef_min,coef_max)
+        for k in range(self.__list) :
+            pass
+
+
+class Formuler() :
+    def __init__(self,formula) :
+        self.__formula:Formula = formula
+        super().__init__(formula)
+    
+    def mutation(self) :
+        if random.uniform(0,1) > mutation_rate :
+            return
+        else :
+            selected_dim = random.choice(self.__formula.get_dim())
+            coef = self.__formula.get_coef(selected_dim)
+            self.__formula.add_term(selected_dim + random.random(-mutation_dim_range,mutation_dim_range + 1),coef + random.uniform(-mutaiton_coef_range,mutaiton_coef_range))
+            self.__formula.zero_check()
+
---
+++ oldMain.py
@@ -0,0 +1,73 @@
+import random
+
+levelupCost = 1
+
+class Formula() :
+    def __init__(self,*formula) :
+        """
+        coefficient:係数
+        dimention:次数
+        formula:[xの係数,xの次数]
+        """
+        self.__formula = list(formula)
+    
+    def calc(self,x) :
+        ans = 0
+        for k in range(len(self.__formula)) :
+            ans += self.__formula[k][0] * (x ** self.__formula[k][1])
+        return ans
+    
+    def getFormula(self) :
+        return self.__formula
+
+    def getDimension(self) :
+        dimentions = []
+        for k in self.__formula :
+            dimentions.append(k[1])
+        return dimentions
+
+    def getCoefs(self) :
+        coefs = []
+        for k in self.__formula :
+            coefs.append(k[0])
+        return coefs
+    
+    def changeCoef(self,dimention,change) :
+        if dimention not in self.getDimention :
+            raise ValueError()
+        dimentionNum = self.getDimention.index(dimention)
+        self.__formula[dimentionNum][0] += change
+
+    def changeDimention(self,dimention)
+
+
+
+class Earth() :
+    def __init__(self,limit,sample) :
+        self.limit = limit
+        self.list = []
+        self.sample:Formula = sample
+
+    def randomGen(self,num,dimCenter,dimRange,coefMin,coefMax):
+        """
+        --range--ave--range--
+        整数次数
+        """
+        dim_min = dimCenter - dimRange
+        dim_max = dimCenter + dimRange
+        tempList = []
+        for k in range(dim_min,dim_max + 1) :
+            tempList.append([k,random.uniform(coefMin,coefMax)])
+        for k in range(num) :
+            self.list.append(Formuler(Formula(*tempList)))
+
+
+class Formuler(Formula) :
+    def mutation(self,rate) :
+        if random.uniform(0,1) < rate :
+            self.Formula
+
+
+f = Formula([3,2],[2,1],[1,0])
+print(f.calc(1))
+print(f.getCoefs())
