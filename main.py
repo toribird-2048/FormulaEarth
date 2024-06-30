747b156db37f2b2eb6a5b6bd8c67d3e77a73ed30
toribird-2048 <fly-toribird@outlook.jp>
2024-06-30 14:59:17 +0900

    2024/6/30 14:59

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ mutation_rate = 0.5
 mutation_dim_range = 2
 mutaiton_coef_range = 2
 
+def get_keys_from_value(d, val):
+    return [k for k, v in d.items() if v == val]
+
 class Formula() :
     def __init__(self,formula) :
         """
@@ -44,11 +47,29 @@ class Formula() :
         if dim in self.__formula.keys() :
             del self.__formula[dim]
 
-    def zero_check(self):
-        for coef in self.__formula.values() :
-            if coef == 0 :
-                self.del_term(self.__formula.keys()[self.__formula.values().index(coef)])
-    
+    def zero_check(self) :
+        zero_list = get_keys_from_value(self.__formula,0)
+        for dim in zero_list :
+            self.del_term(dim)
+
+
+class Formuler() :
+    def __init__(self,formula) :
+        self.__formula:Formula = formula
+
+    def mutation(self) :
+        if random.uniform(0,1) > mutation_rate :
+            return
+        else :
+            selected_dim = random.choice(self.__formula.get_dim())
+            coef = self.__formula.get_coef(selected_dim)
+            self.__formula.add_term(selected_dim + random.randint(-mutation_dim_range,mutation_dim_range + 1),coef + random.uniform(-mutaiton_coef_range,mutaiton_coef_range))
+            self.__formula.zero_check()
+
+    def get_formula(self) :
+        return self.__formula.get_formula()
+
+
 class Earth() :
     def __init__(self,limit,sample) :
         self.__limit:int = limit
@@ -57,23 +78,15 @@ class Earth() :
 
     def randomGen(self,dim_min,dim_max,coef_min,coef_max) :
         terms = {}
-        for k in range(dim_min,dim_max + 1) :
-            terms[k] = random.uniform(coef_min,coef_max)
-        for k in range(self.__list) :
-            pass
+        for l in range(self.__limit) :
+            for k in range(dim_min,dim_max + 1) :
+                terms[k] = random.uniform(coef_min,coef_max)
+            self.__list.append(Formuler(Formula(terms)))
+
+    def get_list(self) :
+        return [k.get_formula() for k in self.__list]
 
 
-class Formuler() :
-    def __init__(self,formula) :
-        self.__formula:Formula = formula
-        super().__init__(formula)
-    
-    def mutation(self) :
-        if random.uniform(0,1) > mutation_rate :
-            return
-        else :
-            selected_dim = random.choice(self.__formula.get_dim())
-            coef = self.__formula.get_coef(selected_dim)
-            self.__formula.add_term(selected_dim + random.random(-mutation_dim_range,mutation_dim_range + 1),coef + random.uniform(-mutaiton_coef_range,mutaiton_coef_range))
-            self.__formula.zero_check()
-
+Earth = Earth(10,Formula({1:1}))
+Earth.randomGen(1,10,1,10)
+print(Earth.get_list())
